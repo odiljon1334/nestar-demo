53675d0d5fb854cdc4762652c1bbf2a239464d3c
odiljon1334 <odiljon9507@gmail.com>
2024-12-27 02:18:58 +0500

    feat: meta tags integrations on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,19 @@ export default function Document() {
   console.log("DOCUMENT PAGES - ROUTER");
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo"/>
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+        {/* SEO */}
+        <meta name="keyword" content={"nestar , nestar.uz, devex mern, mern nestjs. fullstack"} />
+        <meta name="description" content={
+          "Buy and sell properties anywhere anytime in South Korea. | " +
+          "Покупайте и продавайте недвижимость в любом месте Южной Корее в любое время. |" +
+          "대한민국 어디서나 언제든지 부동산 사고 팔수 있습니다."
+          } />
+      </Head>
       <body>
         <Main />
         <NextScript />
